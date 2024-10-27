0377fcbbd5bed17a587183638ec2f409f00ff146
Manraj Singh <msingh209@mylangara.ca>
2024-10-27 16:13:21 -0700

    Editor

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import StoryIntro from "./components/StoryIntro";
 import StoryScreen from "./components/StoryScreen";
 import Query from "./components/Query";
+import SQLEditor from "./components/SQLEditor";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<StoryScreen />} />
       <Route path="/story" element={<StoryIntro />} />
       <Route path="/query" element={<Query />} />
+      <Route path="/SQLEditor" element={<SQLEditor />} />
     </Routes>
   );
 }
